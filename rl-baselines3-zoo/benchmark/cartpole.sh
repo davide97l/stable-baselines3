bea40e730bb1b9b1420faf1c8dac477cfd247e21
Davide Liu <41103541+davide97l@users.noreply.github.com>
2022-04-01 17:16:12 +0800

    small fix

--- rl-baselines3-zoo/benchmark/cartpole.sh
+++ rl-baselines3-zoo/benchmark/cartpole.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-seeds=(1 2 3 4 5)
-algo=('dqn' 'ppo')
-
-for a in "${algo[@]}"; do
-  for s in "${seeds[@]}"; do
-      python train.py --algo "$a" --env CartPole-v1 --stop-value 195. --seed "$s"
-  done
+for s in 1 2 3 4 5; do
+    python train.py --algo dqn --env CartPole-v1 --stop-value 195. --seed "$s"
+    python train.py --algo ppo --env CartPole-v1 --stop-value 195. --seed "$s"
 done
